2e9178be8555e0f360ae183cecfd30b2d8477391
peterchege <peterchege442@gmail.com>
2022-08-12 02:00:18 +0300

    map module using react-leaflet

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Layout from "components/Layout";
+import Container from "components/Container";
+
+import error_gif from "assets/images/404.gif";
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <Container type="content" className="text-center">
+        <h1>Page Not Found</h1>
+        <img class="gatsby-astronaut" src={error_gif} alt="404" />
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,105 @@
+import React, { useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import L from "leaflet";
+import { Marker, useMap } from "react-leaflet";
+
+import { promiseToFlyTo, getCurrentLocation } from "lib/map";
+
+import Layout from "components/Layout";
+import Map from "components/Map";
+
+import person_icon from "assets/images/personIcon.png";
+
+const LOCATION = {
+  lat: 38.9072,
+  lng: -77.0369,
+};
+const CENTER = [LOCATION.lat, LOCATION.lng];
+const DEFAULT_ZOOM = 2;
+const ZOOM = 10;
+
+const timeToZoom = 2000;
+const timeToOpenPopupAfterZoom = 4000;
+const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
+
+const popupContentHello = `<p>Hello 👋</p>`;
+const popupContentGatsby = `
+  <div class="popup-gatsby">
+    <div class="popup-gatsby-image">
+      <img class="gatsby-astronaut" src=${person_icon} />
+    </div>
+    <div class="popup-gatsby-content">
+      <h1>Found You 🎉 </h1>
+      <p>This is your current position!</p>
+    </div>
+  </div>
+`;
+
+const MapEffect = ({ markerRef }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!markerRef.current || !map) return;
+
+    (async function run() {
+      const popup = L.popup({
+        maxWidth: 800,
+      });
+
+      const location = await getCurrentLocation().catch(() => LOCATION);
+
+      const { current: marker } = markerRef || {};
+
+      marker.setLatLng(location);
+      popup.setLatLng(location);
+      popup.setContent(popupContentHello);
+
+      setTimeout(async () => {
+        await promiseToFlyTo(map, {
+          zoom: ZOOM,
+          center: location,
+        });
+
+        marker.bindPopup(popup);
+
+        setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom);
+        setTimeout(
+          () => marker.setPopupContent(popupContentGatsby),
+          timeToUpdatePopupAfterZoom
+        );
+      }, timeToZoom);
+    })();
+  }, [map, markerRef]);
+
+  return null;
+};
+
+MapEffect.propTypes = {
+  markerRef: PropTypes.object,
+};
+
+const IndexPage = () => {
+  const markerRef = useRef();
+
+  const mapSettings = {
+    center: CENTER,
+    defaultBaseMap: "OpenStreetMap",
+    zoom: DEFAULT_ZOOM,
+  };
+
+  return (
+    <Layout pageName="home">
+      <Helmet>
+        <title>Home Page</title>
+      </Helmet>
+
+      <Map {...mapSettings}>
+        <MapEffect markerRef={markerRef} />
+        <Marker ref={markerRef} position={CENTER} />
+      </Map>
+    </Layout>
+  );
+};
+
+export default IndexPage;
